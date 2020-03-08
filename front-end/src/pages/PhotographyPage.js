49bf7115e3501d2254080d18dcdd813f62674550
Anastasia Egorova <anastasia.eg2302@gmail.com>
2020-03-07 23:29:55 -0500

    Change back-end location, fix edge bug

--- front-end/src/pages/PhotographyPage.js
+++ front-end/src/pages/PhotographyPage.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default class PhotographyPage extends React.Component {
     render() {
         const { params } = this.props.routeMatch
-        console.log(this.props);
         return (
             <div>
                 <h1>PhotographyPage</h1>
--- front-end/src/pages/components/PageWrapper.js
+++ front-end/src/pages/components/PageWrapper.js
@@ -6,47 +6,45 @@ import * as routes from "../../constants/routes";
 export default class PageWrapper extends React.Component {
     render() {
         const Page = this.props.page;
-        const { ...rest } = this.props;
+        const { exact, path, computedMatch } = this.props;
 
         return (
-            <Route
-              {...rest}
-            >
+            <Route exact={exact} path={path}>
                 <NavigationBar />
                 <ul>
-                <li>
-                    <NavLink exact activeClassName="active" to={routes.ABOUT}>
-                    About
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName="active" to={routes.CONTACT}>
-                    Contact
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName="active" to={routes.PHOTOGRAPHY}>
-                    Photography
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName="active" to={routes.PHOTOGRAPHY + "/1"}>
-                    Photography: id
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName="active" to={routes.REALESTATE}>
-                    Real Estate
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName="active" to={routes.REALESTATE + "/1"}>
-                    Real Estate: id
-                    </NavLink>
-                </li>
+                    <li>
+                        <NavLink exact activeClassName="active" to={routes.ABOUT}>
+                        About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to={routes.CONTACT}>
+                        Contact
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to={routes.PHOTOGRAPHY}>
+                        Photography
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to={routes.PHOTOGRAPHY + "/1"}>
+                        Photography: id
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to={routes.REALESTATE}>
+                        Real Estate
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to={routes.REALESTATE + "/1"}>
+                        Real Estate: id
+                        </NavLink>
+                    </li>
                 </ul>
                 <hr />
-                <Page routeMatch={rest.computedMatch} />
+                <Page routeMatch={computedMatch} />
             </Route>
         )
     }
